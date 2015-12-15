c58bdeaa55a449d31fbe3e1e895ca459a125f5fd
Grzegorz Wodo <gwodo@gwodo.(none)>
2015-12-15 12:32:29 +0100

    Capture files

--- src/main/java/se/seamless/socket/api/SocketApi.java
+++ src/main/java/se/seamless/socket/api/SocketApi.java
@@ -1,9 +1,22 @@
 package se.seamless.socket.api;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class SocketApi {
 	public static void main(String[] args) throws IOException {
-		
+		ServerSocket server = new ServerSocket(1234);
+		Socket client = server.accept();
+		BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+			if (line.equals("exit")) {
+				break;
+			}
+		}
 	}
 }
