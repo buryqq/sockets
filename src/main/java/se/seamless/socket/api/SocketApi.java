78017b8ae6121f77610a80c67e6b7f0d509bbe4e
grzegorzwodo <grzegorz.wodo@gmail.com>
2015-12-15 09:48:24 +0100

    Init Socket apiT

---
+++ src/main/java/se/seamless/socket/api/SocketApi.java
@@ -0,0 +1,9 @@
+package se.seamless.socket.api;
+
+import java.io.IOException;
+
+public class SocketApi {
+	public static void main(String[] args) throws IOException {
+		
+	}
+}
---
+++ src/main/java/se/seamless/socket/api/config/SocketApiConfig.java
@@ -0,0 +1,9 @@
+package se.seamless.socket.api.config;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketApiConfig {
+	
+}
